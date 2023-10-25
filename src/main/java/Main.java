56147cf6d63dfbf0db8aa964a9b4b24a7c9fb45b
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-10-25 09:30:39 -0400

    finished, added logic to play again if the player wants to

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,47 +7,62 @@ public class Main {
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int numberToGuess = (int) (Math.random() * 100) +1;
-        int numberGuessed = 0;
-        boolean keepGuesing = true;
+        int numberGuessed;
+        String play;
+        boolean keepGuessing = true;
+        boolean keepPlaying = true;
         int numberOfGuesses = 0;
 
         System.out.println("\n**** Welcome to Guess That Number! ****\n");
 
-        while(keepGuesing){
+        while(keepPlaying) {
 
-            // try-catch block to catch inputs that would break our code (i.e. anything not an int)
-            try {
-                System.out.println("\nPlease enter a number between 1 and 100: ");
-                numberGuessed = input.nextInt();
-            }
-            catch(Exception e){
-                System.out.println("This is not a number!");
-                input.next();
-                continue;
+            int numberToGuess = (int) (Math.random() * 100) + 1;
+
+            while (keepGuessing) {
+
+                // try-catch block to catch inputs that would break our code (i.e. anything not an int)
+                try {
+                    System.out.println("\nPlease enter a number between 1 and 100: ");
+                    numberGuessed = input.nextInt();
+                } catch (Exception e) {
+                    System.out.println("This is not a number!");
+                    input.next();
+                    continue;
+                }
+
+                // guessing logic
+                if (numberGuessed < 1 || numberGuessed > 100) {
+                    System.out.println("A number between 1 and 100, please!");
+                } else {
+                    numberOfGuesses++;
+                    if (numberGuessed > numberToGuess) {
+                        System.out.println("Too high!");
+                    } else if (numberGuessed < numberToGuess) {
+                        System.out.println("Too small!");
+                    } else {
+                        System.out.println("You got it! " + numberGuessed + " is correct! It took " + numberOfGuesses + " tries to guess it.");
+                        keepGuessing = false;
+                    }
+                }
             }
 
-            // guessing logic
-            if(numberGuessed < 1 || numberGuessed > 100){
-                System.out.println("A number between 1 and 100, please!");
+            // logic for playing again
+            System.out.println("Would you like to play again? Y/N: ");
+            play = input.next();
+
+            if (play.equals("N") || play.equals("n")) {
+                System.out.println("Thank you for playing!");
+                keepPlaying = false;
+            } else if (play.equals("Y") || play.equals("y")) {
+                System.out.println("Starting new game...");
+                keepGuessing = true;
+                numberOfGuesses = 0;
+            } else {
+                System.out.println("Y or N not inputted... quitting game...");
+                keepPlaying = false;
             }
-            else{
-                numberOfGuesses++;
-                if(numberGuessed > numberToGuess){
-                    System.out.println("Too high!");
-                }
-                else if(numberGuessed < numberToGuess){
-                    System.out.println("Too small!");
-                }
-                else{
-                    System.out.println("You got it! " + numberGuessed + " is correct! It took " + numberOfGuesses + " tries to guess it.");
-                    keepGuesing = false;
-                }
-            }
+
         }
-
-
-
-        
     }
 }
